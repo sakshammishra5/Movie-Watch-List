f7b87dcefd5fd4d5054302e822244936fd28075d
sakshammishra5 <sakshammishra5thsep@gmail.com>
2023-02-04 22:02:47 +0530

    some changes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
-let watchlistContainer=document.getElementById("watchlistContainer")
-
+let watchlistContainer = document.getElementById("watchlistContainer");
 
 searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+	e.preventDefault();
 	var searchString = searchInput.value;
 	console.log(searchString);
 	fetch(`http://www.omdbapi.com/?s=${searchString}&apikey=abfb5a92`)
@@ -12,9 +11,15 @@ searchBtn.addEventListener("click", (e) => {
 		.then((data) => {
 			console.log(data);
 			console.log(data.Search[0].Title);
-			for(let i=0;i<data.Search.length;i++){
-				watchlistContainer.innerHTML+=
-`<div class="card mb-3" style="max-width: 540px;">
+			for (let i = 0; i < data.Search.length; i++) {
+				fetch(
+					`https://www.omdbapi.com/?i=${data.Search[i].imdbID}&apikey=abfb5a92`
+				)
+					.then((res) => res.json())
+					.then((lol) => {
+						console.log(lol);
+
+						watchlistContainer.innerHTML += `<div class="card mb-3" style="max-width: 540px;">
 		<div class="row g-0">
 		  <div class="col-md-4">
 			<img src="${data.Search[i].Poster}" class="img-fluid rounded-start" alt="...">
@@ -22,18 +27,19 @@ searchBtn.addEventListener("click", (e) => {
 		  <div class="col-md-8">
 			<div class="card-body">
 			  <h5 class="card-title">${data.Search[i].Title}</h5>
-			  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			  <p class="card-text">${lol.Plot}</p>
+			  <p class="card-text"><small class="text-muted">IMDB Rating${lol.imdbRating}</small></p>
 			</div>
 		  </div>
 		</div>
 	  </div>
-`
+	  
+`;
+					});
 			}
 		});
 
-watchlistContainer.innerHTML=
-`<div class="card mb-3" style="max-width: 540px;">
+	watchlistContainer.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
 		<div class="row g-0">
 		  <div class="col-md-4">
 			<img src="https://images.unsplash.com/photo-1675453442429-1ea5b9652743?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" class="img-fluid rounded-start" alt="...">
@@ -47,11 +53,8 @@ watchlistContainer.innerHTML=
 		  </div>
 		</div>
 	  </div>
-`
-        
+`;
 });
-
-
 
 // searchInput.addEventListener("input", (e) => {
 // 	var searchString = e.target.value;
@@ -61,5 +64,5 @@ watchlistContainer.innerHTML=
 // 		.then((data) => {
 // 			console.log(data);
 // 		});
-        
+
 // });
