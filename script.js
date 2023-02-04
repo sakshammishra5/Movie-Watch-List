5c75ac44edc7969db3d80f367c75e1417c3d39b5
sakshammishra5 <sakshammishra5thsep@gmail.com>
2023-02-04 17:56:36 +0530

    input search resolved

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
-searchInput.addEventListener("input", (e) => {
-	var searchString = e.target.value;
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+	var searchString = searchInput.value;
 	console.log(searchString);
 	fetch(`http://www.omdbapi.com/?s=${searchString}&apikey=abfb5a92`)
 		.then((res) => res.json())
 		.then((data) => {
 			console.log(data);
 		});
-
+        
 });
+
+
+
+// searchInput.addEventListener("input", (e) => {
+// 	var searchString = e.target.value;
+// 	console.log(searchString);
+// 	fetch(`http://www.omdbapi.com/?s=${searchString}&apikey=abfb5a92`)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			console.log(data);
+// 		});
+        
+// });
