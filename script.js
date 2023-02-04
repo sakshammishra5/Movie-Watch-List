6c71c3594c715e938451a8db02cdfb6c5ef707bb
sakshammishra5 <sakshammishra5thsep@gmail.com>
2023-02-04 16:56:20 +0530

    input from search bar

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let searchInput=document.getElementById("searchInput")
-let searchBtn=document.getElementById("searchBtn")
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
 
-searchInput.addEventListener("input" ,e=>{
-    var searchString=e.target.value
-    console.log(searchString);
-})
+searchInput.addEventListener("input", (e) => {
+	var searchString = e.target.value;
+	console.log(searchString);
+	fetch(`http://www.omdbapi.com/?s=${searchString}&apikey=abfb5a92`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+		});
 
-
-fetch(`http://www.omdbapi.com/?$=&apikey=abfb5a92`)
-.then(res=>res.json())
-.then(data=>{
-    console.log(data);
-})
+});
